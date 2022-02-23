97d28fadae353ec0ef5dc2cbc68a991005f13211
YUEUN328 <yelub@naver.com>
2022-02-23 21:33:16 +0900

    import 수정

--- PopcornBox/src/main/java/com/spring/pjt/controller/MovieController.java
+++ PopcornBox/src/main/java/com/spring/pjt/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.spring.pjt.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
