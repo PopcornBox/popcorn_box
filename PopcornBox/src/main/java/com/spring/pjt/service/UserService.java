a025b75542213485b4223b717fbf8f057a1bf431
vankoreaseoul <91598430+vankoreaseoul@users.noreply.github.com>
2022-02-23 13:31:54 +0900

    오류 수정

--- PopcornBox/src/main/java/com/spring/pjt/service/UserService.java
+++ PopcornBox/src/main/java/com/spring/pjt/service/UserService.java
@@ -13,10 +13,7 @@ public interface UserService {
 	User readUserByNickname(String user_nickname);
     int resetPwd(User user);
     int validateKey(User user);
-    
-    // 회원 정보 조회
-    User userInfo(String SignInUserNickname);
-    
+   
     // 회원 정보 수정
     void userInfoUpdate(User user);
     
