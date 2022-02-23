f98c42c273bc1c9897e0e8eaf28f5d322247c9e1
YUEUN328 <yelub@naver.com>
2022-02-23 18:20:52 +0900

    간편가입 후 자동로그인 구현

--- PopcornBox/src/main/java/com/spring/pjt/interceptor/SignedInInterceptor.java
+++ PopcornBox/src/main/java/com/spring/pjt/interceptor/SignedInInterceptor.java
@@ -28,32 +28,31 @@ public class SignedInInterceptor implements HandlerInterceptor {
 //		log.info("      session: {}", request.getSession());
 		
 		HttpSession session = request.getSession(); // 세션 생성		
-		String state = (String) session.getAttribute("state");
 		
 		if (request.getMethod().equals("GET")) {
 			log.info("req uri: {}", request.getRequestURI());
 			if (!request.getRequestURI().equals("/pjt/user/kakaologin")) { // 카카오 로그인이 아니면
 				return; // GET 방식에서는 interceptor가 할 일이 없음.(로그인 페이지 form을 보여줌)
-			} else { // 카카오 로그인이면
-					
-//				String signInUserNickname = (String) session.getAttribute("signInUserNickname");
-				if (session.getAttribute("signInUserNickname") != null) {
+			} else { // 카카오 로그인이면					
+				if (session.getAttribute("signInUserNickname") != null) { 
 					log.info("signInUserNickname : {}", session.getAttribute("signInUserNickname"));
 					log.info("signInUserPosition : {}", session.getAttribute("signInUserPosition"));
 					log.info("accessToken : {}", session.getAttribute("accessToken"));
 					log.info("state : {}", session.getAttribute("state"));
 					
 					// 원래 이동하려고 했던 페이지(state)로 redirect
+					String state = (String) session.getAttribute("state");
 					if (state != null && !state.equals("")) {
 						response.sendRedirect(state);
-						session.removeAttribute(state);
+						session.removeAttribute(state); // 이후 필요 없으므로 세션에서 삭제
 						log.info("state 제거 후 session.state : {}", session.getAttribute(state));
 					} else {
 						response.sendRedirect("/pjt");
 					}
 					return;
 					
-				} else { // 회원가입되지 않은 이용자면 간편가입으로 이동
+				} else { // 회원가입되지 않은 이용자는 간편가입으로 이동
+					log.info("email : {}", session.getAttribute("email"));
 					response.sendRedirect("/pjt/user/simpleRegister");
 					return;
 				}
@@ -61,32 +60,57 @@ public class SignedInInterceptor implements HandlerInterceptor {
 			}
 		}
 		
-		// 로그인 성공 후 이동할 페이지(input type="hidden"에서 찾음)
-		String targetUrl = request.getParameter("url");
+		log.info("req uri: {}", request.getRequestURI());
 		
-		Object user = modelAndView.getModel().get("signInUser");
-		if (user != null) { // 테이블에 아이디/비밀번호가 일치하는 사용자가 있는 경우 -> 로그인 성공
-			// 세션에 로그인 사용자 넘버, 닉네임, 등급 저장
-			session.setAttribute("signInUserNickname", ((User) user).getUser_nickname());
-			session.setAttribute("signInUserPosition", ((User) user).getUser_position());
-			log.info((String) session.getAttribute("signInUserNickname"));
-		    log.info((String) session.getAttribute("signInUserPosition"));
+		if (!request.getRequestURI().equals("/pjt/user/simpleRegister")) { // 간편가입 후 로그인이 아니면
+			// 로그인 성공 후 이동할 페이지(input type="hidden"에서 찾음)
+			String targetUrl = request.getParameter("url");
 			
-			// 원래 이동하려고 했던 페이지(targetUrl)로 redirect
-			if (targetUrl != null && !targetUrl.equals("")) {
-				response.sendRedirect(targetUrl);
-			} else {
-				response.sendRedirect("/pjt");
+			Object user = modelAndView.getModel().get("signInUser");
+			if (user != null) { // 테이블에 아이디/비밀번호가 일치하는 사용자가 있는 경우 -> 로그인 성공
+				// 세션에 로그인 사용자 닉네임, 등급 저장
+				session.setAttribute("signInUserNickname", ((User) user).getUser_nickname());
+				session.setAttribute("signInUserPosition", ((User) user).getUser_position());
+				log.info((String) session.getAttribute("signInUserNickname"));
+			    log.info((String) session.getAttribute("signInUserPosition"));
+				
+				// 원래 이동하려고 했던 페이지(targetUrl)로 redirect
+				if (targetUrl != null && !targetUrl.equals("")) {
+					response.sendRedirect(targetUrl);
+				} else {
+					response.sendRedirect("/pjt");
+				}
+				
+			} else { // 테이블에 아이디/비밀번호가 일치하는 사용자가 없는 경우 -> 로그인 실패 
+				// 로그인과 관련된 메시지를 세션에 저장.
+				String msg = (String) modelAndView.getModel().get("msg");
+				if (msg != "") {
+					session.setAttribute("msg", msg);
+				}
+				// 로그인 페이지(signin GET)로 redirect
+				response.sendRedirect("/pjt/user/signin?url=" + UriUtils.encode(targetUrl, "UTF-8"));
 			}
 			
-		} else { // 테이블에 아이디/비밀번호가 일치하는 사용자가 없는 경우 -> 로그인 실패 
-			// 로그인과 관련된 메시지를 세션에 저장.
-			String msg = (String) modelAndView.getModel().get("msg");
-			if (msg != "") {
-				session.setAttribute("msg", msg);
+		} else { // 간편가입 후 자동 로그인의 경우
+			if (session.getAttribute("signInUserNickname") != null) { 
+				log.info("signInUserNickname : {}", session.getAttribute("signInUserNickname"));
+				log.info("signInUserPosition : {}", session.getAttribute("signInUserPosition"));
+				log.info("accessToken : {}", session.getAttribute("accessToken"));
+				log.info("state : {}", session.getAttribute("state"));
+				
+				// 원래 이동하려고 했던 페이지(state)로 redirect
+				String state = (String) session.getAttribute("state");
+				if (state != null && !state.equals("")) {
+					response.sendRedirect(state);
+					session.removeAttribute(state); // 이후 필요 없으므로 세션에서 삭제
+					log.info("state 제거 후 session.state : {}", session.getAttribute(state));
+				} else {
+					response.sendRedirect("/pjt");
+				}
+				return;
 			}
-			// 로그인 페이지(signin GET)로 redirect
-			response.sendRedirect("/pjt/user/signin?url=" + UriUtils.encode(targetUrl, "UTF-8"));
+				
 		}
+		
 	}
 }
--- PopcornBox/src/main/webapp/WEB-INF/views/user/simpleRegister.jsp
+++ PopcornBox/src/main/webapp/WEB-INF/views/user/simpleRegister.jsp
@@ -133,7 +133,7 @@
 	    <section class="checkout spad">
 	        <div class="container">
 	            <div class="checkout__form">
-	                <form action="./register" method="post">
+	                <form action="./simpleRegister" method="post">
 	                    <div class="row">
 	                        <div class="col-lg-8 col-md-6">
 	                            <h6 class="checkout__title">안녕하세요, 간편가입 페이지입니다.</h6>
@@ -158,51 +158,14 @@
 									<div class="invalid_repwd">비밀번호가 일치하지 않습니다.</div>	                                
 	                            </div>
 	                            <div class="col-lg-12 text-center">
-	                            <button type="submit" class="primary-btn">간편가입</button>	                            
+	                            	<button type="submit" class="primary-btn">간편가입</button>	                            
 	                            </div>
-	
-	
-	
-	                      <!--  <div class="col-lg-4 col-md-6">
-	                            <div class="checkout__order">
-	                                <h4 class="order__title">Your order</h4>
-	                                <div class="checkout__order__products">Product <span>Total</span></div>
-	                                <ul class="checkout__total__products">
-	                                    <li>01. Vanilla salted caramel <span>$ 300.0</span></li>
-	                                    <li>02. German chocolate <span>$ 170.0</span></li>
-	                                    <li>03. Sweet autumn <span>$ 170.0</span></li>
-	                                    <li>04. Cluten free mini dozen <span>$ 110.0</span></li>
-	                                </ul>
-	                                <ul class="checkout__total__all">
-	                                    <li>Subtotal <span>$750.99</span></li>
-	                                    <li>Total <span>$750.99</span></li>
-	                                </ul>
-	                                <div class="checkout__input__checkbox">
-	                                    <label for="acc-or">
-	                                        Create an account?
-	                                        <input type="checkbox" id="acc-or">
-	                                        <span class="checkmark"></span>
-	                                    </label>
-	                                </div>
-	                                <p>Lorem ipsum dolor sit amet, consectetur adip elit, sed do eiusmod tempor incididunt
-	                                ut labore et dolore magna aliqua.</p>
-	                                <div class="checkout__input__checkbox">
-	                                    <label for="payment">
-	                                        Check Payment
-	                                        <input type="checkbox" id="payment">
-	                                        <span class="checkmark"></span>
-	                                    </label>
-	                                </div>
-	                                <div class="checkout__input__checkbox">
-	                                    <label for="paypal">
-	                                        Paypal
-	                                        <input type="checkbox" id="paypal">
-	                                        <span class="checkmark"></span>
-	                                    </label>
-	                                </div>
-	                                <button type="submit" class="site-btn">PLACE ORDER</button>
-	                            </div>
-	                        </div>-->
+	                            <div>
+	                            	<input type="hidden" id="user_id" name="user_id" value="${email}" />
+	                            </div>      
+	                            <div>
+	                            	<input type="hidden" id="user_email" name="user_email" value="${email}" />
+	                            </div>               
 	                    </div>
 	                </form>
 	            </div>
