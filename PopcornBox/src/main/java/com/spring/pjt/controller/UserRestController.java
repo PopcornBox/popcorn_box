d294096791e673e8be04798d013704266f7b1a8d
YUEUN328 <yelub@naver.com>
2022-03-01 11:16:29 +0900

    import

--- PopcornBox/src/main/java/com/spring/pjt/controller/UserRestController.java
+++ PopcornBox/src/main/java/com/spring/pjt/controller/UserRestController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ public class UserRestController {
 	
 	@Autowired private UserService userService;
 	@Autowired private UserDao userDao;
+	@Autowired private BCryptPasswordEncoder passwordEncoder;
 	
 	@RequestMapping(value = "/{user_no}", method = RequestMethod.POST)
 	public ResponseEntity<Map<Object, Object>> checkDuplicate(@PathVariable(name = "user_no") int user_no, @RequestBody String user_nickname) {
